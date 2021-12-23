9b5d14f74cbc930e55efdb9e5dae19c140fa67c9
mingmoth <mingmoth@gmail.com>
2021-12-23 13:55:57 +0800

    add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,33 +1,57 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const pageLimit = 9
 
 const restController = {
   getRestaurants: (req, res) => {
+    let offset = 0
     const cateQuery = {}
     let categoryId = ''
-    if(req.query.categoryId) {
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+    if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       cateQuery.CategoryId = categoryId
     }
-    return Restaurant.findAll({include: Category, where: cateQuery}).then(restaurants => {
-      const data = restaurants.map(restaurant => ({
-        ...restaurant.dataValues,
-        description: restaurant.dataValues.description.length > 50 ? restaurant.dataValues.description.substring(0, 50) + '...' : restaurant.dataValues.description,
-        categoryName: restaurant.Category.name
+    Restaurant.findAndCountAll({ 
+      include: Category, 
+      where: cateQuery, 
+      offset: offset,
+      limit: pageLimit
+    }).then(result => {
+      const page = Number(req.query.page) || 1
+      const pages = Math.ceil(result.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+
+      const data = result.rows.map(r => ({
+        ...r.dataValues,
+        description: r.dataValues.description.length > 50 ? r.dataValues.description.substring(0, 50) + '...' : r.dataValues.description,
+        categoryName: r.dataValues.Category.name
       }))
       Category.findAll({
         raw: true,
         nest: true
       }).then(categories => {
-        return res.render('restaurants', { restaurants: data, categories: categories, categoryId: categoryId })  
+        return res.render('restaurants', {
+          restaurants: data,
+          categories: categories,
+          categoryId: categoryId,
+          page: page,
+          totalPage: totalPage,
+          prev: prev,
+          next: next
+        })
       })
-      
+
     })
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, {include: Category}).then(restaurant => {
-      return res.render('restaurant', {restaurant: restaurant.toJSON()})
+    return Restaurant.findByPk(req.params.id, { include: Category }).then(restaurant => {
+      return res.render('restaurant', { restaurant: restaurant.toJSON() })
       // const data = ({
       //   ...restaurant.dataValues,
       //   categoryName: restaurant.dataValues.Category.name
